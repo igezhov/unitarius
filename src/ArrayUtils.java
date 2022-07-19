35d1df86c62e0579795ee583686fbb641c1dcb41
Igor Ezhov <ig.ezh0ff@gmail.com>
2022-07-19 20:43:13 +0300

    juggler + Euclids algo

--- src/ArrayUtils.java
+++ src/ArrayUtils.java
@@ -9,14 +9,36 @@ public class ArrayUtils {
      * @return результирующий массив
      */
     public static int[] cyclicMoveIntArray(int[] intArray, int position) {
+        int oldPosition;
+        int newPosition;
         int temp;
-        for (int a = 0; a < position; a++) {
-            temp = intArray[0];
-            for (int i = 0; i < intArray.length - 1; i++) {
-                intArray[i] = intArray[i + 1];
+        if (position != 0) {
+            for (int i = 0; i < getGCD(intArray.length, position); i++) {
+                temp = intArray[i];
+                oldPosition = i;
+
+                while (true) {
+                    newPosition = oldPosition + position;
+                    if (newPosition >= intArray.length) {
+                        newPosition = newPosition - intArray.length;
+                    }
+
+                    if (newPosition == i)
+                        break;
+
+                    intArray[oldPosition] = intArray[newPosition];
+                    oldPosition = newPosition;
+                }
+                intArray[oldPosition] = temp;
             }
-            intArray[intArray.length - 1] = temp;
         }
         return intArray;
+    }
+
+    private static int getGCD(int n1, int n2) {
+        if (n2 == 0) {
+            return n1;
+        }
+        return getGCD(n2, n1 % n2);
     }
 }
